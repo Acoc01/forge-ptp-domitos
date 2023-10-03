ee71f25fe48b3e6fcb3520995525fbc2cf4cdc4e
rVergaraA <63213116+Acoc01@users.noreply.github.com>
2023-10-03 08:11:07 -0300

    Initial Commit

---
+++ domitos/src/main/java/com/example/demo/DomitosApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomitosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomitosApplication.class, args);
+	}
+
+}
